3ae21191f688cc703fac6b40a4812e3b1a4b81b4
Olivier Chatry <olivier@fieldap.com>
2017-12-17 15:24:18 +0100

    better lodding

--- app/components/svg-image/component.js
+++ app/components/svg-image/component.js
@@ -5,21 +5,31 @@ export default Ember.Component.extend({
 	onDidInsertElement:Ember.on("didInsertElement", function() {
 		const transform	= this.element.ownerSVGElement.createSVGTransform()
 		const svgns			= "http://www.w3.org/2000/svg";
-		const rect			= document.createElementNS(svgns, 'rect');
-		const image			= this.$('image')[0]
-		const parent		= image.parentNode
-		let 	currentDisplay = image
-		const bbox			= image.getBBox()		
-		rect.setAttributeNS(null, 'height', this.height);
-		rect.setAttributeNS(null, 'width', this.width);
-    rect.setAttributeNS(null, 'fill', '#'+Math.round(0xffffff * Math.random()).toString(16));
+		const lod0			= document.createElementNS(svgns, 'image');
+		const lod1			= document.createElementNS(svgns, 'image');
+		const lod2			= document.createElementNS(svgns, 'rect');
+			
+		const parent		= this.$('g')[0]
+
+		lod0.setAttributeNS(null, 'height', this.height);
+		lod0.setAttributeNS(null, 'width', this.width);
+    lod0.setAttributeNS('http://www.w3.org/1999/xlink','href', `${this.baseUrl}-00.svg`);
+
+		lod1.setAttributeNS(null, 'height', this.height);
+		lod1.setAttributeNS(null, 'width', this.width);
+    lod1.setAttributeNS('http://www.w3.org/1999/xlink', 'href', `${this.baseUrl}-01.svg`);
 		
-		const	targetArea = 100
+		lod2.setAttributeNS(null, 'height', this.height);
+		lod2.setAttributeNS(null, 'width', this.width);
+    lod2.setAttributeNS(null, 'fill', '#E4BD00');
+
+		parent.appendChild(lod0)
+		let 	currentDisplay = lod0
 		
 		const handleLodding = (w,h) => {
-			const area = this.width * w * this.height * h 
-			const display = area < targetArea ? rect : image
-			if (display !== currentDisplay) {
+			const area = this.width * w * this.height * h
+			const display = (area > 450) ? lod0 : (area > 100) ? lod1 : lod2
+			if (display !== currentDisplay) {	
 				parent.replaceChild(display, currentDisplay)
 				currentDisplay = display
 			}
